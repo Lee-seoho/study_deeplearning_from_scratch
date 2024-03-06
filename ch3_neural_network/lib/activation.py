3713dabba51bce37c88d882382370fee01fc8afc
Lee-seoho <stout259@gmail.com>
2024-03-06 11:53:30 +0900

    first commit

---
+++ ch3_neural_network/lib/activation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+def softmax2(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T 
